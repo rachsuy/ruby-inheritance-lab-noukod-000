391ade2c228f559b882277fd4c61620110e3aac4
Rachele Suy <rachelesuy@gmail.com>
2019-05-23 05:42:11 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,4 @@
 class User 
-  attr_reader :firstname, :lastname
-  attr_writer :firstname, :lastname
+  attr_accessor :firstname, :lastname
   
-  def initialize(firstname, lastname)
-    @firstname = firstname
-    @lastname = lastname
-  end
 end
